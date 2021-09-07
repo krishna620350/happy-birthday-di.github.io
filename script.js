84b0e59387b83ea469823d2fd2c2ddc879e3eb00
krishna620350 <manishkumar620350@gmail.com>
2021-09-07 10:44:15 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#para", {
+  strings: [
+    "“Count your life by smiles, not tears. Count your age by friends, not years. Happy birthday!”",
+    "“Happy birthday! I hope all your birthday wishes and dreams come true.”",
+    "“May the joy that you have spread in the past come back to you on this day. Wishing you a very happy birthday!”",
+    "“Happy birthday to someone who is smart, gorgeous, funny and reminds me a lot of myself… from one fabulous chick to another!”",
+    "“You are only young once, but you can be immature for a lifetime. Happy birthday!”",
+    "“On your birthday, I thought of giving you the cutest gift in the world. But then I realized that is not possible because you yourself are the cutest gift in the world.”",
+  ],
+  typeSpeed: 80,
+  backSpeed: 60,
+  loop: true,
+});
